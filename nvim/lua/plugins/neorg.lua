0f53fcf295c1b284ae4bd44fe0fe694675e42d8c
Raphael Ladinig <raphaelladinig07@gmail.com>
2024-03-29 09:44:35 +0100

    nvim

--- nvim/lua/plugins/neorg.lua
+++ nvim/lua/plugins/neorg.lua
@@ -1,41 +1,15 @@
 return {
-	"nvim-neorg/neorg",
-	event = "VeryLazy",
-	build = ":Neorg sync-parsers",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-treesitter/nvim-treesitter",
-		"nvim-cmp",
-	},
-	config = function()
-		require("neorg").setup({
-			load = {
-				["core.defaults"] = {},
-				["core.concealer"] = {
-					config = {
-						folds = true,
-						icon_preset = "diamond",
-					},
-				},
-				["core.dirman"] = {
-					config = {
-						workspaces = {
-							notes = "~/Notes",
-						},
-						default_workspace = "notes",
-						index = "index.norg",
-						use_popup = false,
-					},
-				},
-				["core.completion"] = { config = { engine = "nvim-cmp", name = "[Norg]" } },
-				["core.integrations.nvim-cmp"] = {},
-				["core.keybinds"] = {
-					config = {
-						default_keybinds = true,
-						neorg_leader = "<Leader><Leader>",
-					},
-				},
-			},
-		})
-	end,
+    -- {
+    --     "vhyrro/luarocks.nvim",
+    --     priority = 1000,
+    --     config = true,
+    -- },
+    -- {
+    --     "nvim-neorg/neorg",
+    --     dependencies = { "luarocks.nvim" },
+    --     config = function()
+    --         require("neorg").setup({
+    --         })
+    --     end,
+    -- }
 }
