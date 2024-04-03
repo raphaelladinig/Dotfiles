e7a7f044b27f7f630c1f9a93b91aa3c3f869bd80
Raphael Ladinig <raphaelladinig07@gmail.com>
2024-04-03 11:42:10 +0200

    update

--- nvim/lua/plugins/lspsaga.lua
+++ nvim/lua/plugins/lspsaga.lua
@@ -1,6 +1,5 @@
 return {
 	"nvimdev/lspsaga.nvim",
-	event = "LspAttach",
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter",
 		"nvim-tree/nvim-web-devicons",
--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,7 +1,6 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	event = "VeryLazy",
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter-textobjects",
 		"windwp/nvim-ts-autotag",
