57a5a43276f9aacc2f329c45cb1a917ee0380850
Raphael Ladinig <raphaelladinig07@gmail.com>
2024-04-09 14:13:29 +0200

    catpuccin

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,7 +1,7 @@
 return {
 	"catppuccin/nvim",
-    priority = 1000,
-    config = function ()
-        vim.cmd("colorscheme catppuccin-mocha")
-    end
+	priority = 1000,
+	config = function()
+		vim.cmd("colorscheme catppuccin-mocha")
+	end,
 }
