e89604dd7988dd0b11b36889044afb3cd46080fc
Raphael Ladinig <raphaelladinig07@gmail.com>
2024-04-10 16:20:38 +0200

    fix

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -26,9 +26,9 @@ return {
         end
 
         vim.keymap.set("n", "<leader>a", function()
-            harpoon:list():append()
+            harpoon:list():add()
         end)
-        vim.keymap.set("n", "<leader>h", function()
+        vim.keymap.set("n", "<C-h>", function()
             toggle_telescope(harpoon:list())
         end, { desc = "Open harpoon window" })
     end,
