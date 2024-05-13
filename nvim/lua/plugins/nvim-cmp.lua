fbfb0c36ec6b3e4126aa750be7c9113d9b2c96ca
Raphael Ladinig <raphaelladinig07@gmail.com>
2024-05-13 21:01:37 +0200

    dbee cmp

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -1,95 +1,102 @@
 return {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-cmdline",
-        "onsails/lspkind.nvim",
-    },
-    config = function()
-        local cmp = require("cmp")
+	"hrsh7th/nvim-cmp",
+	dependencies = {
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-cmdline",
+		"onsails/lspkind.nvim",
+		{
+			"MattiasMTS/cmp-dbee",
+			dependencies = {
+				{ "kndndrj/nvim-dbee" },
+			},
+		},
+	},
+	config = function()
+		local cmp = require("cmp")
 
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require("luasnip").lsp_expand(args.body)
-                end,
-            },
-            window = {
-                completion = {
-                    border = "rounded",
-                    winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
-                    zindex = 1001,
-                    scrolloff = 0,
-                    col_offset = 0,
-                    side_padding = 1,
-                    scrollbar = false,
-                },
-                documentation = {
-                    border = "rounded",
-                    winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
-                    zindex = 1001,
-                    scrolloff = 0,
-                    col_offset = 0,
-                    side_padding = 1,
-                    scrollbar = false,
-                },
-            },
-            mapping = {
-                ["<Tab>"] = cmp.mapping.select_next_item(),
-                ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-                ["<C-Space>"] = cmp.mapping.complete({}),
-                ["<C-e>"] = cmp.mapping.abort(),
-                ["<CR>"] = cmp.mapping.confirm({ select = true }),
-            },
-            sources = cmp.config.sources({
-                { name = "nvim_lsp" },
-                { name = "luasnip" },
-                { name = "buffer" },
-                { name = "path" },
-            }),
-            formatting = {
-                fields = { "kind", "abbr", "menu" },
-                format = function(entry, vim_item)
-                    local kind = require("lspkind").cmp_format({
-                        mode = "symbol",
-                        maxwidth = 50,
-                        menu = {
-                            nvim_lsp = "[LSP]",
-                            luasnip = "[LuaSnip]",
-                            buffer = "[Buffer]",
-                            path = "[Path]",
-                        },
-                    })(entry, vim_item)
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body)
+				end,
+			},
+			window = {
+				completion = {
+					border = "rounded",
+					winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
+					zindex = 1001,
+					scrolloff = 0,
+					col_offset = 0,
+					side_padding = 1,
+					scrollbar = false,
+				},
+				documentation = {
+					border = "rounded",
+					winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
+					zindex = 1001,
+					scrolloff = 0,
+					col_offset = 0,
+					side_padding = 1,
+					scrollbar = false,
+				},
+			},
+			mapping = {
+				["<Tab>"] = cmp.mapping.select_next_item(),
+				["<S-Tab>"] = cmp.mapping.select_prev_item(),
+				["<C-Space>"] = cmp.mapping.complete({}),
+				["<C-e>"] = cmp.mapping.abort(),
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
+			},
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+				{ name = "buffer" },
+				{ name = "path" },
+				{ name = "cmp-dbee" },
+			}),
+			formatting = {
+				fields = { "kind", "abbr", "menu" },
+				format = function(entry, vim_item)
+					local kind = require("lspkind").cmp_format({
+						mode = "symbol",
+						maxwidth = 50,
+						menu = {
+							nvim_lsp = "[LSP]",
+							luasnip = "[LuaSnip]",
+							buffer = "[Buffer]",
+							path = "[Path]",
+						},
+					})(entry, vim_item)
 
-                    local strings = vim.split(kind.kind, "%s", { trimempty = true })
-                    kind.kind = (strings[1] or "") .. " "
+					local strings = vim.split(kind.kind, "%s", { trimempty = true })
+					kind.kind = (strings[1] or "") .. " "
 
-                    return kind
-                end,
-            },
-        })
-        cmp.setup.cmdline("/", {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = {
-                { name = "buffer" },
-            },
-        })
-        cmp.setup.cmdline(":", {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = cmp.config.sources({
-                { name = "path" },
-            }, {
-                {
-                    name = "cmdline",
-                    option = {
-                        ignore_cmds = { "Man", "!" },
-                    },
-                },
-            }),
-        })
-    end,
+					return kind
+				end,
+			},
+		})
+		cmp.setup.cmdline("/", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = {
+				{ name = "buffer" },
+			},
+		})
+		cmp.setup.cmdline(":", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = cmp.config.sources({
+				{ name = "path" },
+			}, {
+				{
+					name = "cmdline",
+					option = {
+						ignore_cmds = { "Man", "!" },
+					},
+				},
+			}),
+		})
+	end,
 }
