f02c8fa18fa23e184e2b104992910b5bca7163e8
Raphael Ladinig <raphaelladinig07@gmail.com>
2024-06-05 11:31:18 +0200

    update waybar qute and hyprland

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -8,6 +8,25 @@ c: ConfigContainer = c  # type: ignore[name-defined]
 
 config.load_autoconfig(False)
 
+c.backend = "webengine"
+
+c.colors.webpage.preferred_color_scheme = "dark"
+
+c.auto_save.session = True
+c.session.lazy_restore = True
+
+c.content.pdfjs = True
+
+c.scrolling.bar = "always"
+c.scrolling.smooth = True
+
+c.tabs.tooltips = False
+c.tabs.favicons.show = "never"
+
+c.fonts.default_family = "CaskaydiaCove Nerd Font Mono"
+c.fonts.default_size = "14pt"
+c.fonts.hints = "10pt CaskaydiaCove Nerd Font Mono"
+
 if not os.path.exists(config.configdir / "theme.py"):
     theme = "https://raw.githubusercontent.com/catppuccin/qutebrowser/main/setup.py"
     with urlopen(theme) as themehtml:
@@ -17,19 +36,3 @@ if os.path.exists(config.configdir / "theme.py"):
     import theme
 
     theme.setup(c, "mocha", True)
-
-c.colors.webpage.preferred_color_scheme = "dark"
-c.colors.webpage.darkmode.enabled = True
-
-c.auto_save.session = True
-c.session.lazy_restore = True
-
-c.content.pdfjs = True
-
-c.scrolling.bar = "always"
-
-c.tabs.tooltips = False
-
-c.fonts.default_family = "CaskaydiaCove Nerd Font Mono"
-c.fonts.default_size = "14pt"
-c.fonts.hints = "10pt CaskaydiaCove Nerd Font Mono"
