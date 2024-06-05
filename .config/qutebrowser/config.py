4938e6efa097ec6b403face748b31b0e9f47262f
Raphael Ladinig <raphaelladinig07@gmail.com>
2024-06-05 14:58:02 +0200

    update qute

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -6,27 +6,29 @@ from qutebrowser.config.config import ConfigContainer
 config: ConfigAPI = config  # type: ignore[name-defined]
 c: ConfigContainer = c  # type: ignore[name-defined]
 
+# disable autoconfig
 config.load_autoconfig(False)
 
+# general settings
 c.backend = "webengine"
-
 c.colors.webpage.preferred_color_scheme = "dark"
-
+c.url.default_page = "about:blank"
+c.url.start_pages = ["about:blank"]
+c.window.title_format = "{current_title}"
+c.tabs.title.format = "{index}: {audio}{current_title}"
 c.auto_save.session = True
 c.session.lazy_restore = True
-
 c.content.pdfjs = True
-
-c.scrolling.bar = "always"
+c.scrolling.bar = "never"
 c.scrolling.smooth = True
-
 c.tabs.tooltips = False
 c.tabs.favicons.show = "never"
-
+c.tabs.indicator.width = 0
 c.fonts.default_family = "CaskaydiaCove Nerd Font Mono"
-c.fonts.default_size = "14pt"
+c.fonts.default_size = "15pt"
 c.fonts.hints = "10pt CaskaydiaCove Nerd Font Mono"
 
+# theme
 if not os.path.exists(config.configdir / "theme.py"):
     theme = "https://raw.githubusercontent.com/catppuccin/qutebrowser/main/setup.py"
     with urlopen(theme) as themehtml:
@@ -36,3 +38,7 @@ if os.path.exists(config.configdir / "theme.py"):
     import theme
 
     theme.setup(c, "mocha", True)
+
+# keybindings
+config.bind(',v', 'spawn mpv {url}')
+config.bind(',V', 'hint links spawn mpv {hint-url}')
