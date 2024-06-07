06f8c2eb25fa9c8e531414a286ca13ee7bd0ef14
Raphael Ladinig <raphaelladinig07@gmail.com>
2024-06-07 20:04:20 +0200

    fix fonts

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -2,4 +2,13 @@ return {
 	"catppuccin/nvim",
 	name = "catppuccin",
 	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			styles = {
+				comments = { "italic" },
+				conditionals = { "italic" },
+			},
+			show_end_of_buffer = true,
+		})
+	end,
 }
